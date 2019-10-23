1d630001f2a7ef863e79dc9d0ae7d755233e301e
edavidaja <e.david.aja@gmail.com>
2019-10-23 15:47:34 -0400

    kein monorepo

---
+++ src/Fetch.svelte
@@ -0,0 +1,14 @@
+<script>
+  import { onMount } from "svelte";
+  let data = [];
+  let url = "http://127.0.0.1:52335/predict";
+  onMount(async function() {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data)
+    });
+    const json = await response.json();
+    data = json;
+  });
+</script>
